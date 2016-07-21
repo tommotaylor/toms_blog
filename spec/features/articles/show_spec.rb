1de500c4e763a29abeaadaeb836ac93bc91cf6da
Tom Taylor <tommotaylor@gmail.com>
2016-07-21 11:05:26 +0200

    Add load more link for comments on show pages

--- spec/features/articles/show_spec.rb
+++ spec/features/articles/show_spec.rb
@@ -11,4 +11,17 @@ feature "Articles: Show" do
     expect(page).to have_no_content(other_article.body)
     expect(page).to have_no_content(other_article.body)
   end
+
+  context "with comments" do
+    let!(:comments) do
+      30.times { Fabricate :comment, commentable: article }
+    end
+    scenario "has 'Load more comments' link to load more comments", :js do
+      visit article_path(article)
+      default_per_page = 15 # set in Article.rb
+      expect(page).to have_css(".comment", count: default_per_page)
+      click_link "Load more comments..."
+      expect(page).to have_css(".comment", count: 2 * default_per_page)
+    end
+  end
 end
